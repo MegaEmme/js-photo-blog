d97782a05440a14c51841fb031f94f385f1e6bfd
MegaEmme <mikmusco@gmail.com>
2025-03-14 16:21:13 +0100

    feat: solved request / create comments

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
+// recupero i dati da API
 const endpoint = "https://lanciweb.github.io/demo/api/pictures/";
 console.log(endpoint);
-
+// creo elemento dashboard
 const dashBoard = document.querySelector('.row');
 console.log(dashBoard);
-
+// chiamo l'API con axios
 axios.get(endpoint)
+// creo una card che attinge da API
     .then(response =>{
         const result = response.data;
         console.log(result);
+
+        let newCard = '';
+
+        result.forEach(element => {
+            newCard += createHTML(element)
+        });
+
+        dashBoard.innerHTML=newCard;
+
     })
+// creo messaggio di errore
     .catch(error =>{
         console.error(error);
         dashBoard.innerHTML=(`<h2 class="text-center">ERRORE</h2>`);
     })
-
+//funzione per creare eleemtni HTML
 function createHTML(element){
 
-    element={title, date, url};
+    const{title, date, url} = element;
 
     return `<div class="col col-lg-4 col-md-6 col-sm-12 mb-4">
                 <div class="card">                        
